073dbd27a84f66d6903e566108e6387e9d36d75f
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-07-24 12:36:06 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const driversArray =['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = (driversArray) => {
+    return driversArray.slice(0, 2)
+}
+const returnLastTwoDrivers = (driversArray) => {
+    return driversArray.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+function createFareMultiplier(integer){
+    return function multiplyFare(fare){
+        return fare*integer
+  }
+}
+function fareDoubler(fare){
+    return fare*2
+}
+function fareTripler(fare){
+    return fare*3
+}
+function selectDifferentDrivers(driversArray,returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(driversArray)
+}
+
+
